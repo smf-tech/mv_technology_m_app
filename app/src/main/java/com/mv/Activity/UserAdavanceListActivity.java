0a3d78edf053b012bbdb52bee6236c86b5f4dab3
Rohit Gujar <rohit.gujar@nanostuffs.com>
2018-03-26 12:23:09 +0530

    Rohit Team Mgmnt 26-03-2018 12.20

---
+++ app/src/main/java/com/mv/Activity/UserAdavanceListActivity.java
@@ -0,0 +1,162 @@
+package com.mv.Activity;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mv.Adapter.AdavanceAdapter;
+import com.mv.Model.Adavance;
+import com.mv.Model.User;
+import com.mv.R;
+import com.mv.Retrofit.ApiClient;
+import com.mv.Retrofit.ServiceRequest;
+import com.mv.Utils.Constants;
+import com.mv.Utils.LocaleManager;
+import com.mv.Utils.PreferenceHelper;
+import com.mv.Utils.Utills;
+import com.mv.databinding.ActivityUserAdavanceListBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Rohit Gujar on 08-03-2018.
+ */
+
+public class UserAdavanceListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView img_back, img_list, img_logout;
+    private TextView toolbar_title;
+    private RelativeLayout mToolBar;
+    private ActivityUserAdavanceListBinding binding;
+    private AdavanceAdapter adapter;
+    private List<Adavance> mList = new ArrayList<>();
+    private PreferenceHelper preferenceHelper;
+    String mAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_adavance_list);
+        binding.setActivity(this);
+        overridePendingTransition(R.anim.right_in, R.anim.left_out);
+        setActionbar(getString(R.string.expense_list));
+        preferenceHelper = new PreferenceHelper(this);
+
+        if (Utills.isConnected(this))
+            getUserExpenseData();
+    }
+
+    private void getUserExpenseData() {
+        Utills.showProgressDialog(this, "Loading Data", getString(R.string.progress_please_wait));
+        ServiceRequest apiService =
+                ApiClient.getClientWitHeader(this).create(ServiceRequest.class);
+        String url = preferenceHelper.getString(PreferenceHelper.InstanceUrl)
+                + Constants.GetPendingAdavanceData + "?userId=" + User.getCurrentUser(UserAdavanceListActivity.this).getMvUser().getId();
+        apiService.getSalesForceData(url).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Utills.hideProgressDialog();
+                Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+                try {
+                    if (response != null && response.isSuccess()) {
+                        String str = response.body().string();
+                        if (str != null && str.length() > 0) {
+                            JSONObject object = new JSONObject(str);
+                            if (object.has("salaries") && !(object.getString("salaries").equalsIgnoreCase("null"))) {
+                                mList = Arrays.asList(gson.fromJson(object.getString("salaries"), Adavance[].class));
+                            }
+                            if (object.has("action")) {
+                                mAction = object.getString("action");
+                            }
+                            setRecyclerView();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Utills.hideProgressDialog();
+
+            }
+        });
+    }
+
+    private void setRecyclerView() {
+        adapter = new AdavanceAdapter(this, mList);
+        binding.rvExpense.setAdapter(adapter);
+        binding.rvExpense.setHasFixedSize(true);
+        binding.rvExpense.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_back:
+                finish();
+                overridePendingTransition(R.anim.left_in, R.anim.right_out);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.left_in, R.anim.right_out);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleManager.setLocale(base));
+    }
+
+    private void setActionbar(String Title) {
+
+        mToolBar = (RelativeLayout) findViewById(R.id.toolbar);
+        toolbar_title = (TextView) findViewById(R.id.toolbar_title);
+        String str = Title;
+        if (Title != null && Title.contains("\n"))
+            str = Title.replace("\n", " ");
+        toolbar_title.setText(str);
+        img_back = (ImageView) findViewById(R.id.img_back);
+        img_back.setVisibility(View.VISIBLE);
+        img_back.setOnClickListener(this);
+        img_list = (ImageView) findViewById(R.id.img_list);
+        img_list.setVisibility(View.GONE);
+        img_list.setOnClickListener(this);
+        img_logout = (ImageView) findViewById(R.id.img_logout);
+        img_logout.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setRecyclerView();
+    }
+
+
+}
