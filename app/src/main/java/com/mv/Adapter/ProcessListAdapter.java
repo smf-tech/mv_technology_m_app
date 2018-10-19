c6323eeef17cd22a96d1910c960a38f9e53212a1
Koustubh Karmalkar <koustubh_karmalkar@persistent.com>
2018-10-19 16:06:37 +0530

    Fixed Fatal Exception: java.lang.IndexOutOfBoundsException

--- app/src/main/java/com/mv/Adapter/ProcessListAdapter.java
+++ app/src/main/java/com/mv/Adapter/ProcessListAdapter.java
@@ -2,7 +2,6 @@ package com.mv.Adapter;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -32,70 +31,68 @@ import java.util.List;
 /**
  * Created by nanostuffs on 26-09-2017.
  */
-
 public class ProcessListAdapter extends RecyclerView.Adapter<ProcessListAdapter.MyViewHolder> {
 
     private final List<TaskContainerModel> resultList;
-    ArrayList<ArrayList<Task>> taskArrayList = new ArrayList<>();
-    PreferenceHelper preferenceHelper;
+    private ArrayList<ArrayList<Task>> taskArrayList = new ArrayList<>();
+    private PreferenceHelper preferenceHelper;
     private ProcessListActivity mContext;
 
-    Gson gson;
-    Type listType;
+    private Gson gson;
+    private Type listType;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView txtCommunityName, textViewColor;
-        public LinearLayout layout,arrowLay,deletelay;
-        ImageView arrowimg,deleteimg;
 
+        TextView txtCommunityName, textViewColor;
+        LinearLayout layout;
+        LinearLayout arrowLay;
+        ImageView arrowImg;
 
         public MyViewHolder(View view) {
             super(view);
-            txtCommunityName = (TextView) view.findViewById(R.id.txtTemplateName);
-            textViewColor = (TextView) view.findViewById(R.id.temp_color);
-            layout = (LinearLayout) view.findViewById(R.id.layoutTemplate);
-            arrowLay = (LinearLayout) view.findViewById(R.id.lay_delete);
-            arrowimg = (ImageView) view.findViewById(R.id.row_img);
-        //    deletelay = (LinearLayout) view.findViewById(R.id.lay_delete_new);
-        //    deleteimg = (ImageView) view.findViewById(R.id.row_img_delete);
-            layout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (taskArrayList.size() > 0) {
-                        preferenceHelper.insertString(Constants.UNIQUE, resultList.get(getAdapterPosition()).getUnique_Id());
-                        if (preferenceHelper.getBoolean(Constants.IS_LOCATION)) {
-                            preferenceHelper.insertBoolean(Constants.NEW_PROCESS, false);
-                            Intent openClass = new Intent(mContext, ProcessDeatailActivity.class);
-                            openClass.putParcelableArrayListExtra(Constants.PROCESS_ID, taskArrayList.get(getAdapterPosition()));
-                            //  openClass.putExtra("stock_list", resultList.get(getAdapterPosition()).get(0));
-                            mContext.startActivity(openClass);
-                            mContext.overridePendingTransition(R.anim.right_in, R.anim.left_out);
-                        } else {
-                            preferenceHelper.insertBoolean(Constants.NEW_PROCESS, false);
-                            Intent openClass = new Intent(mContext, ProcessDeatailActivity.class);
-                            openClass.putParcelableArrayListExtra(Constants.PROCESS_ID, taskArrayList.get(getAdapterPosition()));
-                            //  openClass.putExtra("stock_list", resultList.get(getAdapterPosition()).get(0));
-                            mContext.startActivity(openClass);
-                            mContext.overridePendingTransition(R.anim.right_in, R.anim.left_out);
-                        }
 
+            txtCommunityName = view.findViewById(R.id.txtTemplateName);
+            textViewColor = view.findViewById(R.id.temp_color);
+            layout = view.findViewById(R.id.layoutTemplate);
+            arrowLay = view.findViewById(R.id.lay_delete);
+            arrowImg = view.findViewById(R.id.row_img);
+
+            layout.setOnClickListener(view1 -> {
+                if (taskArrayList.size() > 0) {
+                    preferenceHelper.insertString(Constants.UNIQUE, resultList.size() > getAdapterPosition() ?
+                            resultList.get(getAdapterPosition()).getUnique_Id() : "");
+
+                    if (preferenceHelper.getBoolean(Constants.IS_LOCATION)) {
+                        preferenceHelper.insertBoolean(Constants.NEW_PROCESS, false);
+
+                        Intent openClass = new Intent(mContext, ProcessDeatailActivity.class);
+                        openClass.putParcelableArrayListExtra(Constants.PROCESS_ID, taskArrayList.get(getAdapterPosition()));
+
+                        mContext.startActivity(openClass);
+                        mContext.overridePendingTransition(R.anim.right_in, R.anim.left_out);
                     } else {
+                        preferenceHelper.insertBoolean(Constants.NEW_PROCESS, false);
 
-                        Utills.showToast("No Task Available ", mContext);
+                        Intent openClass = new Intent(mContext, ProcessDeatailActivity.class);
+                        openClass.putParcelableArrayListExtra(Constants.PROCESS_ID, taskArrayList.get(getAdapterPosition()));
+
+                        mContext.startActivity(openClass);
+                        mContext.overridePendingTransition(R.anim.right_in, R.anim.left_out);
                     }
+                } else {
+                    Utills.showToast("No Task Available ", mContext);
                 }
             });
-
-
         }
     }
 
-
     public ProcessListAdapter(List<TaskContainerModel> resultList, Activity context) {
         this.resultList = resultList;
-       this.mContext = (ProcessListActivity) context;
+        this.mContext = (ProcessListActivity) context;
         this.preferenceHelper = new PreferenceHelper(context);
+
         gson = new Gson();
+
         listType = new TypeToken<ArrayList<Task>>() {
         }.getType();
     }
@@ -114,27 +111,13 @@ public class ProcessListAdapter extends RecyclerView.Adapter<ProcessListAdapter.
         ArrayList<Task> tasks = gson.fromJson(resultList.get(position).getTaskListString(), listType);
         taskArrayList.add(tasks);
 
-        //Task template = tasks.get(0);
         Log.d("pos", String.valueOf(position));
-        // holder.txtCommunityName.setText(Utills.getDate(Long.valueOf(tasks.get(0).getTimestamp__c()), "dd/MM/yyyy hh:mm:ss.SSS"));
-        if (resultList.get(position).getHeaderPosition().equals(""))
+        if (resultList.get(position).getHeaderPosition().equals("")) {
             holder.txtCommunityName.setText(Utills.getDate(Long.valueOf(tasks.get(0).getTimestamp__c()), "dd/MM/yyyy hh:mm:ss.SSS"));
-        else if (resultList.get(position).getHeaderPosition().equals("") )
-            holder.txtCommunityName.setText(Utills.getDate(Long.valueOf(tasks.get(0).getTimestamp__c()), "dd/MM/yyyy hh:mm:ss.SSS"));
-        else
+        } else {
             holder.txtCommunityName.setText(resultList.get(position).getHeaderPosition());
+        }
 
-//        if(resultList.get(position).getIsDeleteAllow()==false) {
-//            holder.arrowLay.setVisibility(View.GONE);
-//        }else {
-//            holder.arrowimg.setBackgroundResource(R.drawable.form_delete);
-//            holder.arrowLay.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    showFormDeletePopUp(position);
-//                }
-//            });
-//        }
         if (tasks.get(0).getIsSave().equals("false")) {
             if (tasks.get(0).getIsApproved__c().equals("false")) {
                 holder.textViewColor.setBackgroundColor(mContext.getResources().getColor(R.color.orange));
@@ -145,30 +128,18 @@ public class ProcessListAdapter extends RecyclerView.Adapter<ProcessListAdapter.
                     holder.textViewColor.setBackgroundColor(mContext.getResources().getColor(R.color.green));
                 }
             }
-            if(resultList.get(position).getIsDeleteAllow()==false) {
+
+            if (!resultList.get(position).getIsDeleteAllow()) {
                 holder.arrowLay.setVisibility(View.GONE);
-            }else {
-                holder.arrowimg.setBackgroundResource(R.drawable.form_delete);
-                holder.arrowLay.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showFormDeletePopUp(position);
-                    }
-                });
+            } else {
+                holder.arrowImg.setBackgroundResource(R.drawable.form_delete);
+                holder.arrowLay.setOnClickListener(v -> showFormDeletePopUp(position));
             }
-          //  holder.arrowimg.setImageResource(R.drawable.arrow);
         } else {
             holder.textViewColor.setBackgroundColor(mContext.getResources().getColor(R.color.red));
-            holder.arrowimg.setBackgroundResource(R.drawable.form_delete);
-            holder.arrowLay.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    showFormDeletePopUp(position);
-                }
-            });
-
+            holder.arrowImg.setBackgroundResource(R.drawable.form_delete);
+            holder.arrowLay.setOnClickListener(v -> showFormDeletePopUp(position));
         }
-        //  holder.eventUserName.setText(String.valueOf(position));
     }
 
     @Override
@@ -176,6 +147,7 @@ public class ProcessListAdapter extends RecyclerView.Adapter<ProcessListAdapter.
         return resultList.size();
     }
 
+    @SuppressWarnings("deprecation")
     private void showFormDeletePopUp(final int position) {
         final AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
 
@@ -189,26 +161,16 @@ public class ProcessListAdapter extends RecyclerView.Adapter<ProcessListAdapter.
         alertDialog.setIcon(R.drawable.logomulya);
 
         // Setting CANCEL Button
-        alertDialog.setButton2(mContext.getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                alertDialog.dismiss();
-                // Write your code here to execute after dialog closed
-              /*  listOfWrongQuestions.add(mPosition);
-                prefObj.insertString( PreferenceHelper.WRONG_QUESTION_LIST_KEY_NAME, Utills.getStringFromList( listOfWrongQuestions ));*/
-            }
-        });
-        // Setting OK Button
-        alertDialog.setButton(mContext.getString(android.R.string.ok), new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                if (resultList.get(0).getIsSave().equals("false")) {
+        alertDialog.setButton2(mContext.getString(android.R.string.cancel), (dialog, which) -> alertDialog.dismiss());
 
-                    mContext.deleteForm(resultList.get(position));
-                    //    AppDatabase.getAppDatabase(mContext).userDao().deleteSingleTask(resultList.get(position).getUnique_Id(), resultList.get(position).getMV_Process__c());
-                    //  ((ProcessListActivity) mContext).getAllProcessData();
-                }else{
-                    AppDatabase.getAppDatabase(mContext).userDao().deleteSingleTask(resultList.get(position).getUnique_Id(), resultList.get(position).getMV_Process__c());
-                    ((ProcessListActivity) mContext).getAllProcessData();
-                }
+        // Setting OK Button
+        alertDialog.setButton(mContext.getString(android.R.string.ok), (dialog, which) -> {
+            if (resultList.get(0).getIsSave().equals("false")) {
+                mContext.deleteForm(resultList.get(position));
+            } else {
+                AppDatabase.getAppDatabase(mContext).userDao().deleteSingleTask(resultList.get(position).getUnique_Id(),
+                        resultList.get(position).getMV_Process__c());
+                mContext.getAllProcessData();
             }
         });
 
