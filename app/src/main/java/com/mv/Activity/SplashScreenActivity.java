282b73d65beedbc2e363b9d5c2b558a2b6bd19c3
Koustubh Karmalkar <koustubh_karmalkar@persistent.com>
2018-10-23 16:49:44 +0530

    Fixed: Fatal Exception: java.lang.NullPointerException - SplashScreenActivity.java line 71

--- app/src/main/java/com/mv/Activity/SplashScreenActivity.java
+++ app/src/main/java/com/mv/Activity/SplashScreenActivity.java
@@ -7,30 +7,17 @@ import android.os.Environment;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 
-import com.mv.BuildConfig;
 import com.mv.Model.User;
 import com.mv.R;
-import com.mv.Retrofit.ApiClient;
-import com.mv.Retrofit.ServiceRequest;
 import com.mv.Utils.Constants;
-import com.mv.Utils.Logger;
 import com.mv.Utils.PreferenceHelper;
 import com.mv.Utils.Utills;
 import com.mv.databinding.ActivityMainBinding;
 
-import org.json.JSONObject;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private PreferenceHelper preference;
-    private static int SPLASH_TIME_OUT = 2000;
+    final static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +26,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
 
-        preference = new PreferenceHelper(this);
-
         // TODO Remove this code after build
+        PreferenceHelper preference = new PreferenceHelper(this);
         if (preference.getBoolean(PreferenceHelper.FIRSTTIME_V_2_7)) {
             preference.clearPrefrences(PreferenceHelper.UserData);
             preference.insertBoolean(PreferenceHelper.FIRSTTIME_V_2_7, false);
@@ -60,78 +46,30 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         new Handler().postDelayed(() -> {
             Intent intent;
-            if (User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll() == null
-                    || TextUtils.isEmpty(User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll())) {
-                intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                User.clearUser();
-            } else {
-                if (User.getCurrentUser(SplashScreenActivity.this).getMvUser().getGender() == null
-                        || TextUtils.isEmpty(User.getCurrentUser(SplashScreenActivity.this).getMvUser().getGender())) {
-                    intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
-                    intent.putExtra(Constants.ACTION, Constants.ACTION_EDIT);
+            if (User.getCurrentUser(SplashScreenActivity.this).getMvUser() != null) {
+                if (User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll() == null
+                        || TextUtils.isEmpty(User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll())) {
+                    intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
                     User.clearUser();
                 } else {
-                    intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                    if (User.getCurrentUser(SplashScreenActivity.this).getMvUser().getGender() == null
+                            || TextUtils.isEmpty(User.getCurrentUser(SplashScreenActivity.this).getMvUser().getGender())) {
+                        intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
+                        intent.putExtra(Constants.ACTION, Constants.ACTION_EDIT);
+                        User.clearUser();
+                    } else {
+                        intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                    }
                 }
+            } else {
+                intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
+                intent.putExtra(Constants.ACTION, Constants.ACTION_EDIT);
+                User.clearUser();
             }
 
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
 
         }, SPLASH_TIME_OUT);
-    }
-
-    private void loginToSalesforce() {
-        Utills.showProgressDialog(this);
-        ServiceRequest apiService =
-                ApiClient.getClient().create(ServiceRequest.class);
-
-        apiService.loginSalesforce(BuildConfig.LOGIN_URL, BuildConfig.USERNAME, BuildConfig.PASSWORD,
-                BuildConfig.CLIENT_SECRET, BuildConfig.CLIENT_ID, Constants.GRANT_TYPE,
-                Constants.RESPONSE_TYPE).enqueue(new Callback<ResponseBody>() {
-
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Utills.hideProgressDialog();
-
-                try {
-                    JSONObject obj = new JSONObject(response.body().string());
-                    String access_token = obj.getString("access_token");
-                    String instance_url = obj.getString("instance_url");
-                    String id = obj.getString("id");
-                    String str_id = id.substring(id.lastIndexOf("/") + 1, id.length());
-
-                    Log.e("$$$$$$$$$$", str_id);
-
-                    preference.insertString(PreferenceHelper.AccessToken, access_token);
-                    preference.insertString(PreferenceHelper.InstanceUrl, instance_url);
-                    preference.insertString(PreferenceHelper.SalesforceUserId, str_id);
-                    preference.insertString(PreferenceHelper.SalesforceUsername, BuildConfig.USERNAME);
-                    preference.insertString(PreferenceHelper.SalesforcePassword, BuildConfig.PASSWORD);
-
-                    new Handler().postDelayed(() -> {
-
-                        Intent intent;
-                        if (User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll() == null
-                                || TextUtils.isEmpty(User.getCurrentUser(SplashScreenActivity.this).getMvUser().getRoll())) {
-                            intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                        } else {
-                            intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-                        }
-
-                        startActivity(intent);
-
-                    }, SPLASH_TIME_OUT);
-                } catch (Exception e) {
-                    Log.e("$$ Exception:", e.getMessage());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Utills.hideProgressDialog();
-                Logger.doToast(getString(R.string.error_something_went_wrong), SplashScreenActivity.this);
-            }
-        });
     }
 }
