649f114f585e76c4a79874fe27a094d29bba603f
sagarmahajan <smahajan@mutthafoundation.org>
2019-05-30 10:02:19 +0530

    Notification flow implemented.

--- app/src/main/java/com/mv/Service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/mv/Service/MyFirebaseMessagingService.java
@@ -15,8 +15,18 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.mv.Activity.AccountSectionActivity;
+import com.mv.Activity.AssetAllocatedListActivity;
+import com.mv.Activity.AttendanceActivity;
 import com.mv.Activity.CommunityHomeActivity;
+import com.mv.Activity.ExpandableListActivity;
+import com.mv.Activity.HomeActivity;
+import com.mv.Activity.LeaveApprovalActivity;
 import com.mv.Activity.SplashScreenActivity;
+import com.mv.ActivityMenu.GroupsFragment;
+import com.mv.ActivityMenu.ProgrammeManagmentFragment;
+import com.mv.ActivityMenu.TeamManagementFragment;
+import com.mv.ActivityMenu.TrainingCalender;
 import com.mv.Model.Community;
 import com.mv.Model.Notifications;
 import com.mv.Model.User;
@@ -122,46 +132,96 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     //This method is only generating push notification
     //It is same as we did in earlier posts
     private void sendNotification(String messageTitle, String messageBody) {
-        Intent intent = null;
-        if (mId == null) {
-            intent = new Intent(this, SplashScreenActivity.class);
-        } else if (TextUtils.isEmpty(mId)) {
-            intent = new Intent(this, SplashScreenActivity.class);
-        } else {
-            preferenceHelper.insertString(PreferenceHelper.COMMUNITYID, mId);
-            List<Community> list = AppDatabase.getAppDatabase(getApplicationContext()).userDao().getAllCommunities();
-            int position = -1;
-            Community community = new Community();
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).getId().equalsIgnoreCase(mId)) {
-                    position = i;
-                    community = list.get(i);
-                    break;
-                }
-            }
-            if (community != null) {
-                int count;
-                if (community.getCount() == null || TextUtils.isEmpty(community.getCount()))
-                    count = 0;
-                else
-                    count = Integer.parseInt(community.getCount());
-                count = count + 1;
-                community.setCount("" + count);
-                AppDatabase.getAppDatabase(getApplicationContext()).userDao().updateCommunities(community);
-                if (position > 0)
-                    list.remove(position);
-                Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-                String json = gson.toJson(list);
-                intent = new Intent(getApplicationContext(), CommunityHomeActivity.class);
-                intent.putExtra(Constants.TITLE, community.getName());
-                intent.putExtra(Constants.LIST, json);
-            }
+        Intent notificationIntent = null;
+//        if (mId == null) {
+//            notificationIntent = new Intent(this, SplashScreenActivity.class);
+//        } else if (TextUtils.isEmpty(mId)) {
+//            notificationIntent = new Intent(this, SplashScreenActivity.class);
+//        } else if(mId.equals("Attendance")){
+//            notificationIntent = new Intent(this, AttendanceActivity.class);
+//        }
+//        else {
+//            preferenceHelper.insertString(PreferenceHelper.COMMUNITYID, mId);
+//            List<Community> list = AppDatabase.getAppDatabase(getApplicationContext()).userDao().getAllCommunities();
+//            int position = -1;
+//            Community community = new Community();
+//            for (int i = 0; i < list.size(); i++) {
+//                if (list.get(i).getId().equalsIgnoreCase(mId)) {
+//                    position = i;
+//                    community = list.get(i);
+//                    break;
+//                }
+//            }
+//            if (community != null) {
+//                int count;
+//                if (community.getCount() == null || TextUtils.isEmpty(community.getCount()))
+//                    count = 0;
+//                else
+//                    count = Integer.parseInt(community.getCount());
+//                count = count + 1;
+//                community.setCount("" + count);
+//                AppDatabase.getAppDatabase(getApplicationContext()).userDao().updateCommunities(community);
+//                if (position > 0)
+//                    list.remove(position);
+//                Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+//                String json = gson.toJson(list);
+//                notificationIntent = new Intent(getApplicationContext(), CommunityHomeActivity.class);
+//                notificationIntent.putExtra(Constants.TITLE, community.getName());
+//                notificationIntent.putExtra(Constants.LIST, json);
+//            }
+//        }
+
+        switch (mId){
+            case "Attendance":
+                notificationIntent = new Intent(this, AttendanceActivity.class);
+                break;
+            case "Asset":
+                notificationIntent = new Intent(this, AssetAllocatedListActivity.class);
+                break;
+            case "Register":
+                notificationIntent = new Intent(this, HomeActivity.class);
+                break;
+            case "Ho Support":
+                notificationIntent = new Intent(this, GroupsFragment.class);
+                break;
+            case "Process Answer Comment":
+                notificationIntent = new Intent(this, ProgrammeManagmentFragment.class);
+                break;
+            case "Leave":
+                notificationIntent = new Intent(this, LeaveApprovalActivity.class);
+                break;
+            case "User Approve":
+                notificationIntent = new Intent(this, TeamManagementFragment.class);
+                break;
+            case "Calendar":
+                notificationIntent = new Intent(this, TrainingCalender.class);
+                break;
+            case "Comment On Post":
+                notificationIntent = new Intent(this, ProgrammeManagmentFragment.class);
+                break;
+            case "Event":
+                notificationIntent = new Intent(this, TrainingCalender.class);
+                break;
+            case "Return Advance":
+                notificationIntent = new Intent(this, AccountSectionActivity.class);
+                break;
+            case "Salary Deposite":
+                notificationIntent = new Intent(this, AccountSectionActivity.class);
+                break;
+            case "Mulyavardhan Content":
+                notificationIntent = new Intent(this, ExpandableListActivity.class);
+                break;
+            case "Form":
+                notificationIntent = new Intent(this, ProgrammeManagmentFragment.class);
+                break;
+                default :
+                    notificationIntent = new Intent(this, SplashScreenActivity.class);
         }
 
-        if (intent != null) {
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (notificationIntent != null) {
+            notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         }
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
                 PendingIntent.FLAG_ONE_SHOT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
